7084e964ddd1918da09318bf6c3ff6e44da70a72
Sahin Islam <sahinislam878@gmail.com>
2025-07-10 16:56:21 +0530

    Progress: Completed Connecting Java and DB Lecture from Udemy

--- src/DemoJdbc.java
+++ src/DemoJdbc.java
@@ -1,5 +1,10 @@
-public class DemoJdbc {
-    public static void main(String[] args) {
+//import Package
+
+import java.sql.*;
+
+
+public class DemoJdbc{
+    public static void main(String[] args)  throws Exception{
         /*
         * import Package
         * load and register
@@ -9,5 +14,24 @@ public class DemoJdbc {
         * process the results
         * close
         * */
+
+        //load and register
+
+                //This step is Optional
+//        try{
+//            Class.forName("org.postgresql.Driver");
+//        }catch (Exception e){
+//            System.out.println(e);
+//        }
+
+        //create connection
+        String url = "jdbc:postgresql://localhost:5432/demo";
+        String user = "postgres";
+        String passwd = "****";
+
+        Connection con = DriverManager.getConnection(url,user,passwd);
+
+        System.out.println("Connection Established");
+
     }
 }
