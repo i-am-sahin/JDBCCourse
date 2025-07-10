cf116fdcfdce4c7094cc72be269b910dd51b1280
Sahin Islam <sahinislam878@gmail.com>
2025-07-10 16:32:52 +0530

    Start Learning JDBC

---
+++ src/DemoJdbc.java
@@ -0,0 +1,13 @@
+public class DemoJdbc {
+    public static void main(String[] args) {
+        /*
+        * import Package
+        * load and register
+        * create connection
+        * create statement
+        * execute statement
+        * process the results
+        * close
+        * */
+    }
+}
